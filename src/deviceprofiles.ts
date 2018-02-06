f68ee96ed05912ed11c477f9dfd541c074ad0c57
Jay Himanshu Jha <himanshujha19@gmail.com>
2018-02-05 23:33:53 -0800

    Class for Device Profiles

---
+++ src/deviceprofiles.ts
@@ -0,0 +1,40 @@
+import * as rp from 'request-promise';
+import buildRequest from './requestbuilder';
+import { map } from 'bluebird';
+
+interface DeviceComponent {
+  id: string
+  capabilities: Array<String>
+}
+
+export default class DeviceProfiles {
+  personalAccessToken: string;
+
+  constructor(personalAccessToken: string) {
+    this.personalAccessToken = personalAccessToken;
+  }
+
+  listDeviceProfiles() : rp.RequestPromise{
+    return buildRequest(this.personalAccessToken, `deviceprofiles`, 'GET');
+  }
+
+  createDeviceProfile(name: string, components: Array<DeviceComponent>, metadata?: {}) : rp.RequestPromise{
+    let body = {
+      name: name, 
+      components: components,
+      metadata: {}
+    }
+    if (metadata) {
+      body.metadata = {};
+    }
+    return buildRequest(this.personalAccessToken, `deviceprofiles`, 'POST', body);
+  }
+
+  getDeviceProfileDescription(deviceProfileId: string): rp.RequestPromise {
+    return buildRequest(this.personalAccessToken, `deviceprofiles/${deviceProfileId}`, 'GET');
+  }
+
+  deleteDeviceProfile(deviceProfileId: string) : rp.RequestPromise {
+    return buildRequest(this.personalAccessToken, `deviceprofiles/${deviceProfileId}`, 'DELETE');
+  }
+}
