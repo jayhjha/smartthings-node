36e4972bf6d4cc1ee2575f3ec9bd88dd2d535fa3
Jay Himanshu Jha <himanshujha19@gmail.com>
2018-02-06 22:05:35 -0800

    Making DeviceProfiles, Subscriptions and Schedules available through SmartThings module

--- src/SmartThings.ts
+++ src/SmartThings.ts
@@ -1,22 +1,30 @@
-import * as rp from 'request-promise';
-
-import Devices from './devices';
 import Apps from './apps';
+import Devices from './devices';
+import DeviceProfiles from './deviceprofiles';
 import InstalledApps from './installedapps';
 import Locations from './locations';
+import Subscriptions from './subscriptions';
+import Schedules from './schedules';
 
 export class SmartThings {
   protected personalAccessToken: string;
-  public devices : Devices;
+  
   public apps: Apps;
+  public devices : Devices;
+  public deviceProfiles: DeviceProfiles;
   public installedApps: InstalledApps;
   public locations: Locations;
+  public subscriptions: Subscriptions;
+  public schedules: Schedules;
 
   constructor(personalAccessToken: string) {
     this.personalAccessToken = personalAccessToken;
-    this.devices = new Devices(this.personalAccessToken);
     this.apps = new Apps(this.personalAccessToken);
+    this.deviceProfiles = new DeviceProfiles(this.personalAccessToken);
+    this.devices = new Devices(this.personalAccessToken);
     this.installedApps = new InstalledApps(this.personalAccessToken);
     this.locations = new Locations(this.personalAccessToken);
+    this.subscriptions = new Subscriptions(this.personalAccessToken);
+    this.schedules = new Schedules(this.personalAccessToken);
   }
 }
